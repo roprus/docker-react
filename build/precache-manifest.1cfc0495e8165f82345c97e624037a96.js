744b896ecd782d4584762928a1a376262789c728
roprus <13799933+roprus@users.noreply.github.com>
2019-02-03 15:30:10 -0500

    initial commit

---
+++ build/precache-manifest.1cfc0495e8165f82345c97e624037a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ebed590634c1e030144",
+    "url": "/static/js/main.8ebed590.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8ebed590634c1e030144",
+    "url": "/static/css/main.3cf8ef8d.chunk.css"
+  },
+  {
+    "revision": "150535dca4ccdf27d1a4b08fae6bbbac",
+    "url": "/index.html"
+  }
+];
